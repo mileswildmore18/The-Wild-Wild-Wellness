ad117ff36c4ef8c76eb15733447dccda7ac4e46b
Salokin <nicholas.dpoul@gmail.com>
2024-04-17 14:02:58 -0400

    made random recipe selector, then made it push recipe name into function to make new fetch for the actual recipe

--- Assets/js/dietScript.js
+++ Assets/js/dietScript.js
@@ -1,10 +1,10 @@
 let apikey = '7d3bff7bd1mshf07209b4c87620fp1a8bf8jsne5b3b63b54ea';
-let foodDiets = [];
 const loseBFUrl = 'https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByNutrients?limitLicense=false&minProtein=30&maxFat=10';
 const loseWtUrl = 'https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByNutrients?limitLicense=false&minProtein=30&maxFat=10';
 const gainMusUrl = 'https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByNutrients?limitLicense=false&minProtein=30&maxFat=10';
 const mainMusUrl = 'https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByNutrients?limitLicense=false&minProtein=30&maxFat=10';
 
+// Creates the options for the diet dropdown list -N
 function makeDDL () {
     const options = ["Lose Body Fat", "Lose Weight", "Gain Muscle", "Maintain Muscle"];
     for (const option of options) {
@@ -18,6 +18,7 @@ function makeDDL () {
     }
 }
 
+// Causes the dropdown list to display when diet button is hovered over -N
 document.addEventListener('DOMContentLoaded', function() {
     var elems2 = document.querySelectorAll('.dropdown-trigger2');
     M.Dropdown.init(elems2, {
@@ -27,7 +28,7 @@ document.addEventListener('DOMContentLoaded', function() {
     makeDDL();
   }); 
 
-// Adds event listener to the dropdown of the foods to select for the necessary diet for the user's choice
+// Adds event listener to the dropdown, calls for fetch request -N
 document.addEventListener('DOMContentLoaded', () => {
     const dd2 = document.querySelector("#dropdown2");
     dd2.addEventListener('click', (event) => {
@@ -36,7 +37,7 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 });
 
-//This will fetch the selected foods for diet based on user's goals.
+//Calls for individual fetch request based on selection -N
 function fetchFoods(selectedFood) {
     if (selectedFood === "Lose Body Fat") {
         loseBF(selectedFood);
@@ -49,6 +50,7 @@ function fetchFoods(selectedFood) {
     }
 }
 
+// Makes fetch request for option "Lose Body Fat" -N
 function loseBF(selectedFood) {
     console.log(selectedFood);
     fetch(loseBFUrl, {
@@ -62,10 +64,11 @@ function loseBF(selectedFood) {
         return response.json();
     })
     .then(function (data) {
-        console.log(data);
+        getRecipe(data);        
     }) 
 }
 
+// Makes fetch request for option "Lose Weight" -N
 function loseWt(selectedFood) {
     console.log(selectedFood);
     fetch(loseWtUrl, {
@@ -79,10 +82,11 @@ function loseWt(selectedFood) {
         return response.json();
     })
     .then(function (data) {
-        console.log(data);
+        getRecipe(data);        
     })
 }
 
+// Makes fetch request for option "Gain Muscle" -N
 function gainMus(selectedFood) {
     console.log(selectedFood);
     fetch(gainMusUrl, {
@@ -96,10 +100,11 @@ function gainMus(selectedFood) {
         return response.json();
     })
     .then(function (data) {
-        console.log(data);
+        getRecipe(data);        
     })
 }
 
+// Makes fetch request for option "Maintain Muscle" -N
 function mainMus(selectedFood) {
     console.log(selectedFood);
     fetch(mainMusUrl, {
@@ -113,52 +118,37 @@ function mainMus(selectedFood) {
         return response.json();
     })
     .then(function (data) {
-        console.log(data);
+        getRecipe(data);        
     })
 }
 
+// Takes name of recipe and makes new fetch for actual recipe
+// Our original fetch returned the name, img, and nutrient values, but not the actual recipe -N
+function getRecipe(data) {
+    console.log(data);
+    let foodRec = data[Math.floor(Math.random()*data.length)];
+    console.log(foodRec);
+    let recipeName = foodRec.title;
+    console.log(recipeName);
+}
 
-//Fills the card with info from the API fetch and appends to the page
-// function foodCard(chosenFood) {
-//     let dietCard = document.querySelector(".foodCard");
-//     let foodName = document.createElement("p");
-//     let foodTarget = document.createElement("p");
-//     let foodInfo = document.createElement("p");
-//     let foodPhoto = document.createElement("a");
+//Fills the card with info from the API fetch and appends to the page -M
+function foodCard() {
+    let dietCard = document.querySelector(".foodCard");
+    let foodName = document.createElement("p");
+    let foodTarget = document.createElement("p");
+    let foodInfo = document.createElement("p");
+    let foodPhoto = document.createElement("a");
 
-//     foodName.textContent = chosenFood.foodName;
-//     foodTarget.textConent = chosenFood.foodTarget;
-//     foodInfo.textContent = chosenFood.foodInfo;
-//     foodPhoto.setAttribute = ("href", chosenFood.foodPhotoUrl);
+    foodName.textContent = chosenFood.foodName;
+    foodTarget.textConent = chosenFood.foodTarget;
+    foodInfo.textContent = chosenFood.foodInfo;
+    foodPhoto.setAttribute = ("href", chosenFood.foodPhotoUrl);
 
-//     dietCard.appendChild(foodName);
-//     dietCard.appendChild(foodTarget);
-//     dietCard.appendChild(foodInfo);
-//     dietCard.appendChild(foodPhoto);
+    dietCard.appendChild(foodName);
+    dietCard.appendChild(foodTarget);
+    dietCard.appendChild(foodInfo);
+    dietCard.appendChild(foodPhoto);
 
-//     console.log(chosenFood);
-// }
-
-// makeDDL();
-
-// getAPI();
-
-// function getAPI () {
-    // fetch(queryurl, {
-    //     method: 'GET',
-    //     headers: {
-    //     "X-RapidAPI-Host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
-    //     "X-RapidAPI-Key": apikey,
-    //     }
-    // })
-        // .then(function (response) {
-        // return response.json();
-        // })
-//         .then(function (foodDiets) {
-            
-//             for (const foodDiet of foodDiets){
-//                 console.log(foodDiet);
-                
-//             }
-//         });
-// }
+    console.log(chosenFood);
+}
