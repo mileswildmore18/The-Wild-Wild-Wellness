3a245607ba95689f0e089248373da32eceabd693
ellafsd <ellagirin82@gmail.com>
2024-04-18 14:08:15 -0400

    styled navbar

---
+++ Assets/js/sideNav.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var options = {edge:"right"}
+    var instances = M.Sidenav.init(elems, options);
+
+});
